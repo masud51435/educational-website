fccbcbf4b83241d90fa87711af5c93a3cc35efaa
masud51435 <masudrana82955@gmail.com>
2021-10-04 22:22:02 +0600

    second commit

--- src/component/catagories/Catagories.js
+++ src/component/catagories/Catagories.js
@@ -9,19 +9,19 @@ const Catagories = () => {
       .then(res => res.json())
       .then(data => setCatagories(data));
   }, []);
-  
+
   return (
     <div className="container">
       <h1 className="mt-5">Our Featured Services</h1>
       <p className="mb-5">Choose any services you want for your business web application, <br /> your new subject or choose whice subject you like more.</p>
-      <div  className="all-cata">
-      {
-        catagories.map(catagory => <Catagory
-          key={catagory.name}
-          catagory={catagory}
-        ></Catagory>)
-      }
-     </div>
+      <div className="all-data">
+        {
+          catagories.map(catagory => <Catagory
+            key={catagory.name}
+            catagory={catagory}
+          ></Catagory>)
+        }
+      </div>
     </div>
   );
 };
